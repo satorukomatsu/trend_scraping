3b384ef1122db6962fcdb735b53abe0dd3106d9d
satorukomatsu <sk.comiu@gmail.com>
2023-02-15 17:57:53 +0900

    lineへのメッセージ送信ルーティング追加

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
 import fetch from "node-fetch";
 import { stringify } from "csv-stringify/sync";
-import Iconv from "iconv-lite"
-import fs from "fs"
-import jsdom from "jsdom"
+import fs from "fs";
+import line from "@line/bot-sdk";
+import bodyParser from "body-parser";
+import express from "express";
+import jsdom from "jsdom";
+import dotenv from "dotenv";
+dotenv.config()
 const {JSDOM} = jsdom;
+const app = express();
+
+const port = 3003;
+const server = app.listen(port, () => {
+    console.log("server is running ", port);
+});
+
+app.use(bodyParser.json());
+
+// webhookに含まれる情報の取得
+// app.post("/", (req, res, next) => {
+//     console.log(req.body.events.map((handleEvent) => {
+//         return handleEvent.source;
+//     }))
+// })
+
+// LINEへのメッセージ送信（プッシュ）
+app.post("/line/bot/push", async (req, res, next) => {
+    const url = "https://api.line.me/v2/oauth/accessToken"
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    const data = {
+        "grant_type": "client_credentials",
+        "client_id": process.env.CLIENT_ID,
+        "client_secret": process.env.CLIENT_SECRET
+    }
+    const params = new URLSearchParams()
+    Object.keys(data).forEach((key) => params.append(key, data[key]))
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: params
+    }
+    const response = await fetch(url, options)
+    const resTxt = await response.text()
+    const token = JSON.parse(resTxt).access_token
+    const client = new line.Client({
+        channelAccessToken: token
+    });
+    const message = {
+        type: "text",
+        text: "Hello satoru"
+    }
+    client.pushMessage(process.env.USER_ID, message)
+    res.sendStatus(200)
+});
 
 (async () => {
     const url = "https://prtimes.jp/"
@@ -18,7 +69,6 @@ const {JSDOM} = jsdom;
         result.push(!title ? ["nothing"]: {"number": i, "title": title.textContent.trim()})
     }
     const output = stringify(result)
-    const outputSjis = Iconv.encode(output, "Shift_JIS")
     console.log(output)
     fs.writeFileSync("PR_Times.csv", output)
 })();
